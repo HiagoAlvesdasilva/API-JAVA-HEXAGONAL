6652caf7969bc6ee9cd1ce23bcb24d5484e6d402
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2024-11-14 23:41:47 -0300

    Feat: UsuarioController

--- src/main/java/github/com/HiagoAlvesdasilva/acesso_api/adapter/controllers/UsuarioController.java
+++ src/main/java/github/com/HiagoAlvesdasilva/acesso_api/adapter/controllers/UsuarioController.java
@@ -1,2 +1,34 @@
-package github.com.HiagoAlvesdasilva.acesso_api.adapter.controllers;public class UsuarioController {
+package github.com.HiagoAlvesdasilva.acesso_api.adapter.controllers;
+
+import github.com.HiagoAlvesdasilva.acesso_api.adapter.converter.UsuarioConverter;
+import github.com.HiagoAlvesdasilva.acesso_api.adapter.dtos.UsuarioDTO;
+import github.com.HiagoAlvesdasilva.acesso_api.core.domain.Usuario;
+import github.com.HiagoAlvesdasilva.acesso_api.core.ports.UsuarioServicePorts;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api/usuarios")
+@Tag(name = "Usuario", description = "API de usuários")
+public class UsuarioController {
+
+
+    private final UsuarioServicePorts usuarioServicePorts;
+    private final UsuarioConverter usuarioConverter;
+
+    public UsuarioController(UsuarioServicePorts usuarioServicePorts, UsuarioConverter usuarioConverter) {
+        this.usuarioServicePorts = usuarioServicePorts;
+        this.usuarioConverter = usuarioConverter;
+    }
+
+    @Operation(summary = "Criar um novo usuário")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UsuarioDTO create(@RequestBody UsuarioDTO usuarioDTO) {
+        Usuario novoUsuario = usuarioServicePorts.createUsuario(usuarioConverter.toTomain(usuarioDTO));
+        return usuarioConverter.toDto(novoUsuario);
+    }
 }
