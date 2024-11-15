c7f0fa585243e0786f6a81785924512cb3ce4bf5
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2024-11-14 23:38:13 -0300

    Feat: UsuarioDTO

--- src/main/java/github/com/HiagoAlvesdasilva/acesso_api/adapter/dtos/UsuarioDTO.java
+++ src/main/java/github/com/HiagoAlvesdasilva/acesso_api/adapter/dtos/UsuarioDTO.java
@@ -1,16 +1,61 @@
 package github.com.HiagoAlvesdasilva.acesso_api.adapter.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class UsuarioDTO {
     private long id;
     private String nome;
     private String email;
     private String senha;
     private boolean administrador;
+
+    public UsuarioDTO() {
+    }
+
+    public UsuarioDTO(long id, String nome, String email, String senha, boolean administrador) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.administrador = administrador;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(boolean administrador) {
+        this.administrador = administrador;
+    }
 }
