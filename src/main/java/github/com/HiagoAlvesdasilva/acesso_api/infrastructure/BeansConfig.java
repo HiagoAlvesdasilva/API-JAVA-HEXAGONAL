10da00fe86ccfdbca5b2bf689efc3e410481750e
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2024-11-15 22:16:17 -0300

    Feat: Criando BeansConfig

--- src/main/java/github/com/HiagoAlvesdasilva/acesso_api/infrastructure/BeansConfig.java
+++ src/main/java/github/com/HiagoAlvesdasilva/acesso_api/infrastructure/BeansConfig.java
@@ -1,2 +1,23 @@
-package github.com.HiagoAlvesdasilva.acesso_api.infrastructure;public class BeansConfig {
+package github.com.HiagoAlvesdasilva.acesso_api.infrastructure;
+
+import github.com.HiagoAlvesdasilva.acesso_api.core.service.UsuarioServiceImpl;
+import github.com.HiagoAlvesdasilva.acesso_api.core.ports.UsuarioRepositoryPorts;
+import github.com.HiagoAlvesdasilva.acesso_api.core.ports.UsuarioServicePorts;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeansConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public UsuarioServicePorts usuarioServicePortsimp(UsuarioRepositoryPorts usuarioRepositoryPorts){
+        return new UsuarioServiceImpl(usuarioRepositoryPorts);
+    }
+
 }
