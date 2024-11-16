abe9cc2312611ef22a6154c26e1cb0b72c103434
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2024-11-15 22:14:55 -0300

    Feat: Criando UsuarioServicePorts

--- src/main/java/github/com/HiagoAlvesdasilva/acesso_api/core/ports/UsuarioServicePorts.java
+++ src/main/java/github/com/HiagoAlvesdasilva/acesso_api/core/ports/UsuarioServicePorts.java
@@ -1,4 +1,4 @@
-package github.com.HiagoAlvesdasilva.acesso_api.core.domain.service;
+package github.com.HiagoAlvesdasilva.acesso_api.core.ports;
 
 import github.com.HiagoAlvesdasilva.acesso_api.core.domain.Usuario;
 
