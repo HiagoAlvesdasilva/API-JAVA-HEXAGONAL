1f567cf77c454f248097f45648df28bb0924ca2c
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2024-11-15 22:08:45 -0300

    Feat: Class UsuarioConverter

--- src/main/java/github/com/HiagoAlvesdasilva/acesso_api/adapter/converter/UsuarioConverter.java
+++ src/main/java/github/com/HiagoAlvesdasilva/acesso_api/adapter/converter/UsuarioConverter.java
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class UsuarioConverter {
 
-    public Usuario toTomain(UsuarioDTO usuarioDTO){
-        return new Usuario(usuarioDTO.getId(), usuarioDTO.getEmail(), usuarioDTO.getSenha(), usuarioDTO.isAdministrador());
+    public Usuario toTomain(UsuarioDTO usuarioDTO) {
+        return new Usuario(usuarioDTO.getId(), usuarioDTO.getNome(), usuarioDTO.getEmail(), usuarioDTO.getSenha(), usuarioDTO.isAdministrador());
     }
 
-    public UsuarioDTO toDto(Usuario usuario){
-        return new UsuarioDTO(usuario.getId(),null,usuario.getEmail(),usuario.getSenha(),usuario.isAdministrador());
+    public UsuarioDTO toDto(Usuario usuario) {
+        return new UsuarioDTO(usuario.getId(), usuario.getNome(), usuario.getEmail(), usuario.getSenha(), usuario.isAdministrador());
     }
 }
