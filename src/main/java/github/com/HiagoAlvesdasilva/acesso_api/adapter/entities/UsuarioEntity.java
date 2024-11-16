b3db538f8b5959f1a2e96ef051a8421fee848eaa
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2024-11-15 22:09:33 -0300

    Feat: Class UsuarioEntity

--- src/main/java/github/com/HiagoAlvesdasilva/acesso_api/adapter/entities/UsuarioEntity.java
+++ src/main/java/github/com/HiagoAlvesdasilva/acesso_api/adapter/entities/UsuarioEntity.java
@@ -1,2 +1,21 @@
-package github.com.HiagoAlvesdasilva.acesso_api.adapter.entities;public class UsuarioEntity {
+package github.com.HiagoAlvesdasilva.acesso_api.adapter.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "usuario")
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String senha;
+    private boolean administrador;
 }
