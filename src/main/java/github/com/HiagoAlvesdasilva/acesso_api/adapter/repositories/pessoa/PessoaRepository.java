4cab872d7c6ca5b231a34d0efeaa6457173e6e54
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2024-11-16 22:59:08 -0300

    Feat: add Pessoa

--- src/main/java/github/com/HiagoAlvesdasilva/acesso_api/adapter/repositories/pessoa/PessoaRepository.java
+++ src/main/java/github/com/HiagoAlvesdasilva/acesso_api/adapter/repositories/pessoa/PessoaRepository.java
@@ -1,2 +1,7 @@
-package github.com.HiagoAlvesdasilva.acesso_api.adapter.repositories.pessoa;public interface PessoaRepository {
+package github.com.HiagoAlvesdasilva.acesso_api.adapter.repositories.pessoa;
+
+import github.com.HiagoAlvesdasilva.acesso_api.adapter.entities.PessoaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaRepository extends JpaRepository<PessoaEntity, Long> {
 }
