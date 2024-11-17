b794f24a4f1c93b74d1f0afb2b02e5aff487c427
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2024-11-16 22:57:14 -0300

    Feat: add PessoaEntity

--- src/main/java/github/com/HiagoAlvesdasilva/acesso_api/adapter/entities/PessoaEntity.java
+++ src/main/java/github/com/HiagoAlvesdasilva/acesso_api/adapter/entities/PessoaEntity.java
@@ -1,2 +1,32 @@
-package github.com.HiagoAlvesdasilva.acesso_api.adapter.entities;public class PessoaEntity {
+package github.com.HiagoAlvesdasilva.acesso_api.adapter.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_pessoa")
+public class PessoaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pessoa_id")
+    private long pessoaId;
+    private String nome;
+
+    public long getPessoaId() {
+        return pessoaId;
+    }
+
+    public void setPessoaId(long pessoaId) {
+        this.pessoaId = pessoaId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
